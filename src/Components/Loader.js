9f58bdb04691fb8cf018fbdd8a933a340370fe98
“Dhruvin <dhruvinmakwana04@gmail.com>
2022-06-09 20:32:25 -0400

    credits

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,13 +1,19 @@
 
 import * as React from 'react';
 class Loader extends React.Component {
-    render() {
-      return (
-          <div className='loader'>
-              <video src="loader.mp4" muted loop autoPlay ></video>
-              <p className='loader-text'>Please wait while we set up few things for you...</p>
-          </div>
-      );
-    }
+  render() {
+    return (
+      <div className='loader'>
+        <video src="loader.mp4" muted loop autoPlay ></video>
+        <p className='loader-text'>Please wait while we set up few things for you...</p>
+        <p className='loader-text'> Illustrations taken from <a href="https://www.youtube.com/watch?v=eHwR9kTIMeI&ab_channel=PatrikPietschmann">
+          Patrik Pietschmann
+        </a> and  <a href="https://www.remotion.dev/docs/">
+            Remotion</a> </p>
+
+      </div>
+
+    );
   }
+}
 export default Loader;
